c3bbb8070b5384ac64078c1609924159dce40acf
DanstenEM <finaginpv@gmail.com>
2026-02-03 09:01:56 +0400

    Gate interaction by look

--- Assets/Scripts/Interactions/PlayerTriggerInteraction.cs
+++ Assets/Scripts/Interactions/PlayerTriggerInteraction.cs
@@ -8,15 +8,23 @@ public class PlayerTriggerInteraction : MonoBehaviour
 {
     [Header("Inputs")]
     [SerializeField] private ActionToType[] interactActions;
+    [SerializeField] private float lookRayDistance = 4f;
 
-    IInteractable currentInteractable;
+    private readonly System.Collections.Generic.HashSet<IInteractable> nearbyInteractables = new();
+    private IInteractable currentInteractable;
 
     void OnDestroy()
     {
+        ClearCurrentInteractable();
         foreach (var item in interactActions)
         {
             item.Action.action.performed -= OnInteract;
         }
+    }
+
+    private void Update()
+    {
+        UpdateLookedInteractable();
     }
 
     void OnInteract(InputAction.CallbackContext ctx)
@@ -32,30 +40,79 @@ public class PlayerTriggerInteraction : MonoBehaviour
         var trigger = other.GetComponent<IInteractable>();
         if (trigger != null)
         {
-            currentInteractable = trigger;
-
-            var input = interactActions.FirstOrDefault(x => x.KeyActiveType == currentInteractable.keyType);
-            input.Action.action.performed += OnInteract;
-            input.Action.action.Enable();
-
-            var path = input.Action.action.bindings[0];
-
-            currentInteractable.Active(path);
+            nearbyInteractables.Add(trigger);
         }
     }
 
     void OnTriggerExit(Collider other)
     {
         var trigger = other.GetComponent<IInteractable>();
-        if (trigger != null && trigger.Equals(currentInteractable))
+        if (trigger != null)
         {
-            var input = interactActions.FirstOrDefault(x => x.KeyActiveType == currentInteractable.keyType);
-            input.Action.action.performed -= OnInteract;
-            input.Action.action.Disable();
-            currentInteractable.Deactive();
-
-            currentInteractable = null;
+            nearbyInteractables.Remove(trigger);
+            if (trigger.Equals(currentInteractable))
+            {
+                ClearCurrentInteractable();
+            }
         }
+    }
+
+    private void UpdateLookedInteractable()
+    {
+        if (nearbyInteractables.Count == 0)
+        {
+            ClearCurrentInteractable();
+            return;
+        }
+
+        var cameraTarget = Camera.main;
+        if (cameraTarget == null)
+        {
+            ClearCurrentInteractable();
+            return;
+        }
+
+        if (Physics.Raycast(cameraTarget.transform.position, cameraTarget.transform.forward, out var hit, lookRayDistance))
+        {
+            var lookedInteractable = hit.collider.GetComponentInParent<IInteractable>();
+            if (lookedInteractable != null && nearbyInteractables.Contains(lookedInteractable))
+            {
+                if (!ReferenceEquals(currentInteractable, lookedInteractable))
+                {
+                    SetCurrentInteractable(lookedInteractable);
+                }
+                return;
+            }
+        }
+
+        ClearCurrentInteractable();
+    }
+
+    private void SetCurrentInteractable(IInteractable interactable)
+    {
+        ClearCurrentInteractable();
+        currentInteractable = interactable;
+
+        var input = interactActions.FirstOrDefault(x => x.KeyActiveType == currentInteractable.keyType);
+        input.Action.action.performed += OnInteract;
+        input.Action.action.Enable();
+
+        var path = input.Action.action.bindings[0];
+        currentInteractable.Active(path);
+    }
+
+    private void ClearCurrentInteractable()
+    {
+        if (currentInteractable == null)
+        {
+            return;
+        }
+
+        var input = interactActions.FirstOrDefault(x => x.KeyActiveType == currentInteractable.keyType);
+        input.Action.action.performed -= OnInteract;
+        input.Action.action.Disable();
+        currentInteractable.Deactive();
+        currentInteractable = null;
     }
 }
 
