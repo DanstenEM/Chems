b7671e219cbfafc93e73686e549fc57a66d6455f
DanstenEM <finaginpv@gmail.com>
2026-02-08 17:16:18 +0400

    Update DoorExit button to show interaction hint

--- Assets/Scripts/Interactions/DoorExitButton.cs
+++ Assets/Scripts/Interactions/DoorExitButton.cs
@@ -1,7 +1,22 @@
+using System.Globalization;
+using Assets.Scripts.Interactions.Abstract;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class DoorExitButton : TriggerInteractable
+[RequireComponent(typeof(Collider))]
+public class DoorExitButton : MonoBehaviour, IInteractable
 {
+    private static DoorExitButton activeHintOwner;
+    private const string DefaultHintFormat = "PRESS {0} INTERACT";
+
+    [field: SerializeField] public KeyActiveType keyType { get; set; } = KeyActiveType.Tap;
+
+    [Header("Hint")]
+    [SerializeField] private TMP_Text hintText;
+    [SerializeField] private Vector3 hintOffset = new Vector3(0f, 1.2f, 0f);
+    [SerializeField] private string hintFormat = DefaultHintFormat;
+
     [Header("Door")]
     [SerializeField] private Animator doorAnimator;
     [SerializeField] private string openTriggerName = "Open";
@@ -11,13 +26,32 @@ public class DoorExitButton : TriggerInteractable
 
     private void Awake()
     {
+        EnsureHint();
+
         if (doorAnimator == null)
         {
             doorAnimator = GetComponentInParent<Animator>();
         }
     }
 
-    public override void Interact()
+    private void LateUpdate()
+    {
+        if (hintText == null || !hintText.gameObject.activeSelf)
+        {
+            return;
+        }
+
+        var cameraTarget = Camera.main;
+        if (cameraTarget == null)
+        {
+            return;
+        }
+
+        var direction = hintText.transform.position - cameraTarget.transform.position;
+        hintText.transform.rotation = Quaternion.LookRotation(direction);
+    }
+
+    public void Interact()
     {
         if (doorAnimator == null)
         {
@@ -32,5 +66,66 @@ public class DoorExitButton : TriggerInteractable
 
         doorAnimator.SetTrigger(openTriggerName);
         hasOpened = true;
+    }
+
+    public void Active(InputBinding input)
+    {
+        if (hintText == null)
+        {
+            return;
+        }
+
+        if (activeHintOwner != null && activeHintOwner != this)
+        {
+            activeHintOwner.Deactive();
+        }
+
+        activeHintOwner = this;
+
+        var keyLabel = InputControlPath.ToHumanReadableString(
+            input.path,
+            InputControlPath.HumanReadableStringOptions.OmitDevice);
+        if (string.IsNullOrWhiteSpace(keyLabel))
+        {
+            keyLabel = input.path;
+        }
+
+        hintText.text = string.Format(CultureInfo.InvariantCulture, hintFormat, keyLabel.ToUpperInvariant());
+        hintText.gameObject.SetActive(true);
+    }
+
+    public void Deactive()
+    {
+        if (hintText == null)
+        {
+            return;
+        }
+
+        hintText.gameObject.SetActive(false);
+
+        if (activeHintOwner == this)
+        {
+            activeHintOwner = null;
+        }
+    }
+
+    private void EnsureHint()
+    {
+        if (hintText != null)
+        {
+            hintText.gameObject.SetActive(false);
+            return;
+        }
+
+        var hintObject = new GameObject("InteractHint");
+        hintObject.transform.SetParent(transform, false);
+        hintObject.transform.localPosition = hintOffset;
+
+        hintText = hintObject.AddComponent<TextMeshPro>();
+        hintText.text = string.Format(CultureInfo.InvariantCulture, hintFormat, "E");
+        hintText.fontSize = 3.5f;
+        hintText.alignment = TextAlignmentOptions.Center;
+        hintText.color = Color.white;
+        hintText.gameObject.SetActive(false);
     }
 }
