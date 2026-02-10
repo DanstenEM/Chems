d8e68d5bfcb7e631d110c726f1340b42554ededa
DanstenEM <finaginpv@gmail.com>
2026-02-10 15:16:10 +0400

    Revert "Restore drag-drop stacking for identical inventory items"
    This reverts commit 000bd058768282031f9210c6735d4fb792f4525a.

--- Assets/Scripts/UI/Inventory/InventorySlot.cs
+++ Assets/Scripts/UI/Inventory/InventorySlot.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -6,50 +8,20 @@ public class InventorySlot : MonoBehaviour, IDropHandler
 {
     [SerializeField] private Image image;
     [SerializeField] private Color selectColor, notSelectColor;
-
     public void OnDrop(PointerEventData eventData)
     {
-        if (eventData.pointerDrag == null || !eventData.pointerDrag.TryGetComponent(out InventoryItem draggedItem))
+        if(transform.childCount == 0)
         {
-            return;
-        }
+            if(eventData.pointerDrag.TryGetComponent(out InventoryItem item))
+            {
+                if (!IsItemAllowed(item))
+                {
+                    return;
+                }
 
-        if (!IsItemAllowed(draggedItem))
-        {
-            return;
-        }
-
-        var targetItem = GetComponentInChildren<InventoryItem>();
-        if (targetItem == null)
-        {
-            draggedItem.parentAfterDrag = transform;
-            return;
-        }
-
-        if (!CanStackTogether(draggedItem, targetItem))
-        {
-            return;
-        }
-
-        int stackCapacity = Mathf.Max(1, targetItem.itemObj.stackCount);
-        int freeSpace = stackCapacity - targetItem.count;
-        if (freeSpace <= 0)
-        {
-            return;
-        }
-
-        int moveCount = Mathf.Min(draggedItem.count, freeSpace);
-        targetItem.count += moveCount;
-        targetItem.RefrashCount();
-
-        draggedItem.count -= moveCount;
-        if (draggedItem.count <= 0)
-        {
-            Destroy(draggedItem.gameObject);
-        }
-        else
-        {
-            draggedItem.RefrashCount();
+                item.parentAfterDrag = transform;
+                //inventoryItem = item;
+            }
         }
     }
 
@@ -86,15 +58,5 @@ public class InventorySlot : MonoBehaviour, IDropHandler
             InventorySlotMarker.SlotCategory.Weapon => itemObj.category == InventoryItemObj.ItemCategory.Weapon,
             _ => itemObj.category == InventoryItemObj.ItemCategory.Regular
         };
-    }
-
-    private static bool CanStackTogether(InventoryItem draggedItem, InventoryItem targetItem)
-    {
-        if (draggedItem == null || targetItem == null || draggedItem.itemObj == null || targetItem.itemObj == null)
-        {
-            return false;
-        }
-
-        return targetItem.itemObj.isStackable && draggedItem.itemObj == targetItem.itemObj;
     }
 }
